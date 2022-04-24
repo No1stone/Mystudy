c5bd5a888bb4a0483a2a717b654ddea6f65838d2
JangWonSeok <jangws1003@naver.com>
2022-04-24 17:05:12 +0900

    thymeleaf add form add

--- bootwebapp/src/main/java/com/example/bootwebapp/biz/v1/controller/EmployeeController.java
+++ bootwebapp/src/main/java/com/example/bootwebapp/biz/v1/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.example.bootwebapp.db.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -20,12 +22,18 @@ public class EmployeeController {
         return mav;
     }
 
-    @GetMapping(path = "/addEmployee")
+    @GetMapping(path = "/addEmployeeForm")
     public ModelAndView addEmployee(){
         ModelAndView mav = new ModelAndView("add-employee-form");
         Employee ep = new Employee();
         mav.addObject("employee", ep);
         return mav;
+    }
+
+    @PostMapping(path = "saveEmployee")
+    public String saveemployee(@ModelAttribute Employee employee){
+        employeeRepository.save(employee);
+    return "redirect:/list";
     }
 
 }
