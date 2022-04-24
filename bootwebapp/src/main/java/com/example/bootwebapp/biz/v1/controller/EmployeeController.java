6ce04e94f543501b32ed76420e4ba3b48323630e
JangWonSeok <jangws1003@naver.com>
2022-04-24 18:01:51 +0900

    thymeleaf update delete add

--- bootwebapp/src/main/java/com/example/bootwebapp/biz/v1/controller/EmployeeController.java
+++ bootwebapp/src/main/java/com/example/bootwebapp/biz/v1/controller/EmployeeController.java
@@ -3,10 +3,13 @@ package com.example.bootwebapp.biz.v1.controller;
 import com.example.bootwebapp.db.entity.Employee;
 import com.example.bootwebapp.db.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class EmployeeController {
 
     private final EmployeeRepository employeeRepository;
+    private final Logger logger = LoggerFactory.getLogger(EmployeeController.class);
 
     @GetMapping(path = {"/list", "/"})
     public ModelAndView getAllEmployees() {
@@ -30,10 +34,27 @@ public class EmployeeController {
         return mav;
     }
 
-    @PostMapping(path = "saveEmployee")
+    @PostMapping(path = "/saveEmployee")
     public String saveemployee(@ModelAttribute Employee employee){
         employeeRepository.save(employee);
     return "redirect:/list";
+    }
+
+    @GetMapping(path = "/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Long employeeId){
+        logger.info("update - {}", employeeId);
+        ModelAndView mav = new ModelAndView("add-employee-form");
+        Employee employee = employeeRepository.findById(employeeId).orElse(null);
+        mav.addObject("employee", employee);
+        return mav;
+    }
+
+    @GetMapping(path = "/deleteEmployee")
+    public String deleteEmployee(@RequestParam Long employeeId){
+        logger.info("delete - {}", employeeId);
+        employeeRepository.deleteById(employeeId);
+        return "redirect:/list";
+
     }
 
 }
